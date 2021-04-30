ebcc2577c1054459af0cd9f82deab63f2d5cb7e0
xiaochuang50 <wulin50@seu.edu.cn>
2021-04-30 19:33:26 +0800

    third commit

--- src/main/java/com/xiaochuang50/HelloGit.java
+++ src/main/java/com/xiaochuang50/HelloGit.java
@@ -4,5 +4,6 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello github");
+        System.out.println("hello gitee");
     }
 }
