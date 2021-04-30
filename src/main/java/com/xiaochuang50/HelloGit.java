83bee2fce4244b59cbe8c5fab8f46b44301c1f61
xiaochuang50 <wulin50@seu.edu.cn>
2021-04-30 19:39:53 +0800

    hot-fix commit

--- src/main/java/com/xiaochuang50/HelloGit.java
+++ src/main/java/com/xiaochuang50/HelloGit.java
@@ -5,5 +5,6 @@ public class HelloGit {
         System.out.println("hello git");
         System.out.println("hello github");
         System.out.println("hello gitee");
+        System.out.println("hello git4");
     }
 }
